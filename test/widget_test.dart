396b8334250573255cad039389415460a4305557
Maksim Nikolaev <maksim.nikolaev.dev@gmail.com>
2021-07-18 00:37:35 +0300

    updated widget test description

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tic_tac_toe/src/init/init_app.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Find Tic Tac Toe text', (WidgetTester tester) async {
     await tester.pumpWidget(InitApp());
 
     expect(find.text('Tic Tac Toe'), findsOneWidget);
